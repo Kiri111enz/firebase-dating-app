00824d241942bad416ccfd7afca276024d06ae24
Kiri111enz <omelyanenkokirill@gmail.com>
2023-08-09 14:22:37 +0700

    disable inaccessible tabs

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,16 +1,18 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { useRouter} from 'next/router';
-import {SegmentedControl} from '@mantine/core';
+import { SegmentedControl } from '@mantine/core';
+import { ProfileContext } from './RequireAuth';
 
 const Footer: React.FC = () => {
+    const { profileSetUp } = useContext(ProfileContext);
     const router = useRouter();
 
     return (
         <SegmentedControl style={{ position: 'sticky' }} className="top-[100vh]" fullWidth
             value={router.pathname.substring(1)} onChange={(value) => router.push(value)} data={[
-                { value: 'feed', label: 'Feed' },
-                { value: 'likes', label: 'Likes' },
-                { value: 'chats', label: 'Chats', },
+                { value: 'feed', label: 'Feed', disabled: !profileSetUp },
+                { value: 'likes', label: 'Likes', disabled: !profileSetUp },
+                { value: 'chats', label: 'Chats', disabled: !profileSetUp },
                 { value: 'profile', label: 'Profile' }
             ]}/>
     );
--- src/components/RequireAuth.tsx
+++ src/components/RequireAuth.tsx
@@ -1,33 +1,40 @@
-import React, { useState, useEffect, useContext, PropsWithChildren } from 'react';
+import React, { useState, useEffect, createContext, useContext, PropsWithChildren } from 'react';
 import { useRouter } from 'next/router';
 import { Loader } from '@mantine/core';
 import { useAuthState } from 'react-firebase-hooks/auth';
-import { doc, onSnapshot } from 'firebase/firestore';
+import { doc, getDoc, onSnapshot } from 'firebase/firestore';
 import { AppContext } from 'pages/_app';
+
+export const ProfileContext = createContext({ profileSetUp: false });
 
 const RequireAuth: React.FC<PropsWithChildren> = ({ children }) => {
     const { auth, firestore } = useContext(AppContext);
     const [user, loading] = useAuthState(auth);
-    const [profileSetUp, setProfileSetUp] = useState(false);
+    const [profileSetUp, setProfileSetUp] = useState<boolean | null>(null);
     const router = useRouter();
 
     useEffect(() => {
         if (!user)
             return;
 
-        const unsubscribe = onSnapshot(doc(firestore, 'users', user.uid),
-            (doc) => setProfileSetUp(doc.data()?.profileSetUp));
+        const docRef = doc(firestore, 'users', user.uid);
+        getDoc(docRef).then(snapshot => setProfileSetUp(snapshot.data()?.profileSetUp));
+        const unsubscribe = onSnapshot(docRef, (doc) => setProfileSetUp(doc.data()?.profileSetUp));
 
         return () => unsubscribe();
     }, [user]);
 
-    if (loading)
+    if (loading || profileSetUp === null)
         return <Loader className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2" />;
     if (!user)
         void router.push('/auth');
-    if (!profileSetUp)
+    if (!profileSetUp && router.pathname !== '/profile')
         void router.push('/profile');
-    return <>{children}</>;
+    return (
+        <ProfileContext.Provider value={{ profileSetUp }}>
+            {children}
+        </ProfileContext.Provider>
+    );
 };
 
 export default RequireAuth;
