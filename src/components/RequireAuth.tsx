473d913fe8ede55d32d0ba01f9d669f216fd101b
Kiri111enz <omelyanenkokirill@gmail.com>
2023-08-11 22:23:19 +0700

    use useDocumentData hook for profile

--- src/components/RequireAuth.tsx
+++ src/components/RequireAuth.tsx
@@ -2,7 +2,8 @@ import React, { useState, useEffect, createContext, useContext, PropsWithChildre
 import { useRouter } from 'next/router';
 import { Loader } from '@mantine/core';
 import { useAuthState } from 'react-firebase-hooks/auth';
-import { doc, getDoc, onSnapshot } from 'firebase/firestore';
+import { doc, DocumentReference } from 'firebase/firestore';
+import { useDocumentData } from 'react-firebase-hooks/firestore';
 import { AppContext } from 'pages/_app';
 
 export interface Profile {
@@ -13,29 +14,23 @@ export const ProfileContext = createContext<Profile>({} as Profile);
 
 const RequireAuth: React.FC<PropsWithChildren> = ({ children }) => {
     const { auth, firestore } = useContext(AppContext);
-    const [user, loading] = useAuthState(auth);
-    const [profile, setProfile] = useState<Profile | null>(null);
+    const [user, userLoading] = useAuthState(auth);
+    const [profileRef, setProfileRef] = useState<DocumentReference | null>(null);
+    const [profile] = useDocumentData(profileRef);
     const router = useRouter();
 
     useEffect(() => {
-        if (!user)
-            return;
-
-        const docRef = doc(firestore, 'profiles', user.uid);
-        getDoc(docRef).then(snapshot => setProfile(snapshot.data() as Profile));
-        const unsubscribe = onSnapshot(docRef, (doc) => setProfile(doc.data() as Profile));
-
-        return () => unsubscribe();
+        setProfileRef(user ? doc(firestore, 'profiles', user.uid) : null);
     }, [user]);
 
-    if (loading || (user && profile === null))
-        return <Loader className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2" />;
-    if (!user)
+    if (!user && !userLoading)
         void router.push('/auth');
-    else if (!profile!.setUp && router.pathname !== '/profile')
+    else if (userLoading || !profile)
+        return <Loader className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2" />;
+    else if (!profile.setUp && router.pathname !== '/profile')
         void router.push('/profile');
     else return (
-        <ProfileContext.Provider value={ profile! }>
+        <ProfileContext.Provider value={ profile as Profile }>
             {children}
         </ProfileContext.Provider>
     );
