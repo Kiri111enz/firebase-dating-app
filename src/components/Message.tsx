a530e9bf2deef6fd0145c9e0756391fc357d62c2
Kiri111enz <omelyanenkokirill@gmail.com>
2023-08-20 16:05:29 +0700

    fix message text direction

--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -15,7 +15,7 @@ const Message: React.FC<MessageProps> = ({ rtl, className, message, photoURL }) 
     <div style={{ direction: rtl ? 'rtl' : 'ltr' }} className={`flex items-start p-2 ${className}`}>
         <Avatar size="md" radius="xl" src={photoURL}></Avatar>
         <div className={rtl ? 'mr-3' : 'ml-3'}>
-            <Paper shadow="xs" className="px-2 py-1">{message.text}</Paper>
+            <Paper style={{ direction: 'ltr' }} shadow="xs" className="px-2 py-1">{message.text}</Paper>
             <Text size="xs" color="gray" className="italic mt-1">{formatTime(message.createdAt.toDate())}</Text>
         </div>
     </div>
